ce9e2644f442af97e375b0d6310bf053a0ac5e98
Lucas <lucas.gautier@live.fr>
2021-01-15 17:49:42 +0100

    15.01 - 5:50pm

--- src/components/city/index.jsx
+++ src/components/city/index.jsx
@@ -2,43 +2,88 @@ import "./index.scss";
 import React from 'react';
 import city_photo from '../../assets/img/city_photo.png';
 
-const CityPresentation = () => (
-    <section className="Header MyContainer">
+class CityPresentation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      button_one_laville: true,
+      button_two_lestrajets: false,
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick(e) {
+      if (e.target.id === "button_one_laville") {
+        this.setState({
+          button_one_laville: true,
+          color: "#86cebb",
+        })
+        this.setState({
+          button_two_lestrajets: false,
+          color: "#000000",
+        })
+      }
+      if (e.target.id === "button_two_lestrajets") {
+        this.setState({
+          button_one_laville: false,
+          color: "#000000",          
+        })
+        this.setState({
+          button_two_lestrajets: true,
+          color: "#86cebb",
+        })
+      }
+    }
+
+  choosenTownStyle = {border: '1px solid black'};
+
+  
+   render() {
+     return(
+      <section className="Header MyContainer">
       <img src={city_photo} alt="Ville de Chalifert" className="CityPhoto"/>
         <section className="CityDescription">
           <div className="CityDescriptionTitle">
             <div className="MainTitle"> CHALIFERT, </div>
             <div className="Description"> UN PETIT VILLAGE AUTHENTIQUE AU COEUR DE LA VERDURE </div>
           </div>
-
-          <ul className="Robotosmall list-decoration list-decoration--svg">
-            <li>
-              Gare RER/TGV de Chessy à 10 minutes par le bus ligne 24.
-            </li>
-            <li>
-              Paris porte de Bercy à 35 minutes par l’autoroute A4.
-            </li>
-            <li>
-              Centre commercial Val d’Europe à 8 minutes en voiture.
-            </li>
-              {/*
-                <li>
-                  Un charmant village briard bordé par la Marne
-                <li>
-                  Ses vieilles pierres, son église et son écluse.
-                </li>
-                <li>
-                  Ses infrastructures scolaires, culturelles et sportives.
-                </li>
-              */}
-          </ul>
-            <div className="ListChoice">
-              <div className="ListChoiceOne"> La ville </div>
-              <div className="ListChoiceTwo"> Les trajets </div>
-            </div>
-        </section>
+          <div className="conditional-render-section">
+         {
+          this.state.button_one_laville 
+             ? 
+          (<ul className="Robotosmall list-decoration list-decoration--svg">
+            <li> Un charmant village briard bordé par la Marne</li>
+            <li> Ses vieilles pierres, son église et son écluse.</li>
+            <li> Ses infrastructures scolaires, culturelles et sportives.</li>
+            </ul>) 
+            :
+          (<div></div>)
+         }
+         {
+          this.state.button_two_lestrajets
+             ? 
+          (<ul className="Robotosmall list-decoration list-decoration--svg">
+            <li>Gare RER/TGV de Chessy à 10 minutes par le bus ligne 24.</li>
+            <li>Paris porte de Bercy à 35 minutes par l’autoroute A4.</li>
+            <li>Centre commercial Val d’Europe à 8 minutes en voiture.</li>
+          </ul>) 
+             :
+          (<div></div>)
+         }
+         </div>
+         <div className="ListChoice">
+           <div id="button_one_laville" className="ListChoiceOne"
+           onClick={this.handleClick}>La ville</div>
+          
+           <div id="button_two_lestrajets" className="ListChoiceTwo"
+           style={{color: this.state.color }}
+           onClick={this.handleClick}>Les trajets</div>
+         </div>
+         </section>
     </section>
-);
-  
-  export default CityPresentation;
+    )
+   }
+}
+
+export default CityPresentation;
   
--- src/components/offerspatchs/index.jsx
+++ src/components/offerspatchs/index.jsx
@@ -1,72 +1,86 @@
 import "./index.scss";
 import React from 'react';
+import MediaQuery from 'react-responsive'
+
 //import Slider from "react-slick";
 
 const OffersPatch = () => (
-  <section className="MyContainer OffersPatchGlobal">
+  <div>
+    <MediaQuery minWidth={1224}>
+      <MediaQuery minWidth={769}>
+      <section className="MyContainer OffersPatchGlobal">
+        <div className="OffersPatchGreen">
+          <div className="OffersPatchLignOne"> VOTRE STUDIO
+          </div>
+          <div className="OffersPatchLignTwo">
+            <span className="Lign"> </span>
+            <div> À PARTIR DE </div>
+            <span className="Lign"> </span>
+          </div>
+          <div className="OffersPatchLignThree">
+            <div className="OffersPatchLignThreePrice"> 390€ </div>
+            <div className="OffersPatchLignThreePerMonths"> /MOIS </div>
+          </div>
+        </div>
 
-    <div className="OffersPatchGreen">
-      <div className="OffersPatchLignOne"> VOTRE STUDIO
-      </div>
-    
-      <div className="OffersPatchLignTwo">
-        <span className="Lign"> </span>
-          <text> À PARTIR DE </text>
-        <span className="Lign"> </span>
-      </div>
-      
-      <div className="OffersPatchLignThree">
-        <div className="OffersPatchLignThreePrice"> 390€ </div>
-        <div className="OffersPatchLignThreePerMonths"> /MOIS </div>
-      </div>
-    </div>
+        <div className="OffersPatchTurquoize">
+          <div className="OffersPatchLignOne"> VOTRE 2 PIECES </div>
+          <div className="OffersPatchLignTwo">
+            <span className="Lign"> </span>
+            <div> À PARTIR DE </div>
+            <span className="Lign"> </span>
+          </div>
+          <div className="OffersPatchLignThree">
+            <div className="OffersPatchLignThreePrice"> 570 € </div>
+            <div className="OffersPatchLignThreePerMonths"> /MOIS </div>
+          </div>
+        </div>
+        
+        <div className="OffersPatchPurple">
+          <div className="OffersPatchLignOne"> VOTRE 3 PIECES </div>
+          <div className="OffersPatchLignTwo">
+            <span className="Lign"> </span>
+            <div className="OffersPatchLignTwo"> À PARTIR DE </div>
+            <span className="Lign"> </span>
+          </div>
+          <div className="OffersPatchLignThree">
+            <div className="OffersPatchLignThreePrice"> 814 € </div>
+            <div className="OffersPatchLignThreePerMonths"> /MOIS </div>
+          </div>
+        </div>
 
-    <div className="OffersPatchTurquoize">
-      <div className="OffersPatchLignOne"> VOTRE 2 PIECES </div>
-
-      <div className="OffersPatchLignTwo">
-        <span className="Lign"> </span>
-          <text> À PARTIR DE </text>
-        <span className="Lign"> </span>
-      </div>
-
-      <div className="OffersPatchLignThree">
-        <div className="OffersPatchLignThreePrice"> 570 € </div>
-        <div className="OffersPatchLignThreePerMonths"> /MOIS </div>
-      </div>
-    </div>
-    
-    <div className="OffersPatchPurple">
-      <div className="OffersPatchLignOne"> VOTRE 3 PIECES </div>
-
-      <div className="OffersPatchLignTwo">
-        <span className="Lign"> </span>
-          <div className="OffersPatchLignTwo"> À PARTIR DE </div>
-        <span className="Lign"> </span>
-      </div>
-
-      <div className="OffersPatchLignThree">
-        <div className="OffersPatchLignThreePrice"> 814 € </div>
-        <div className="OffersPatchLignThreePerMonths"> /MOIS </div>
-      </div>
-    </div>
-
-
-    <div className="OffersPatchOrange">
-      <div className="OffersPatchLignOne"> VOTRE 4 PIECES </div>
-
-      <div className="OffersPatchLignTwo">
-        <span className="Lign"> </span>
-          <div className="OffersPatchLignTwo"> À PARTIR DE </div>
-        <span className="Lign"> </span>
-      </div>
-
-      <div className="OffersPatchLignThree">
-        <div className="OffersPatchLignThreePrice"> 972 € </div>
-        <div className="OffersPatchLignThreePerMonths"> /MOIS </div>
-      </div>
-    </div>
-  </section>
+        <div className="OffersPatchOrange">
+          <div className="OffersPatchLignOne"> VOTRE 4 PIECES </div>
+          <div className="OffersPatchLignTwo">
+            <span className="Lign"> </span>
+            <div className="OffersPatchLignTwo"> À PARTIR DE </div>
+            <span className="Lign"> </span>
+          </div>
+          <div className="OffersPatchLignThree">
+            <div className="OffersPatchLignThreePrice"> 972 € </div>
+            <div className="OffersPatchLignThreePerMonths"> /MOIS </div>
+          </div>
+        </div>
+      </section>
+     </MediaQuery>
+    </MediaQuery>
+      <MediaQuery minResolution="2dppx">
+        <div>
+          <div className="OffersPatchTurquoize">
+            <div className="OffersPatchLignOne"> VOTRE 2 PIECES </div>
+            <div className="OffersPatchLignTwo">
+              <span className="Lign"> </span>
+              <div> À PARTIR DE </div>
+              <span className="Lign"> </span>
+            </div>
+            <div className="OffersPatchLignThree">
+              <div className="OffersPatchLignThreePrice"> 570 € </div>
+              <div className="OffersPatchLignThreePerMonths"> /MOIS </div>
+            </div>
+          </div>
+        </div>
+    </MediaQuery>
+  </div>
 );
 
 export default OffersPatch;
