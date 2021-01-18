44dc4178b813cf339bd16d8897c0d009ae16146d
Lucas <lucas.gautier@live.fr>
2021-01-18 02:04:11 +0100

    bug fixes + slider on mobile only

--- src/components/introimage/index.jsx
+++ src/components/introimage/index.jsx
@@ -6,7 +6,7 @@ const Introimage = () => (
   <section className="HeaderIntroduction">
     <div className="HeaderIntroductionText ">
       <div className="Dates"> Les 14 et 15 janvier </div>
-      <div className="Title"> GRAND LANCEMENT {/* SI LE TEMPS - AJOUTER INLINE BREAK SUR MOBILE*/} À CHALIFERT </div>
+      <div className="Title"> GRAND LANCEMENT À CHALIFERT </div>
       <div className="Text"> Devenez propriétaire <span>
       aux portes de Val d'Europe </span></div>
       </div>
--- src/components/offerspatchs/index.jsx
+++ src/components/offerspatchs/index.jsx
@@ -1,13 +1,89 @@
 import "./index.scss";
 import React from 'react';
 import MediaQuery from 'react-responsive'
+import { useMediaQuery } from 'react-responsive'
+import Slider from "react-slick";
 
-//import Slider from "react-slick";
+const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery({ maxWidth: 767 })
+  return isMobile ? children : null
+}
+
+class PatchsSlider extends React.Component {
+	render(){
+		let settings = { 
+      className: "OffersPatchSlider",
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      pauseOnHover: true,
+      autoplay: true,
+//      customPaging: i => (
+//      <div className="ft-slick__dots--custom">
+//      <div className="loading" />
+//      </div>),
+		}
+		return (
+			<Slider {...settings} >
+        <div className="OffersPatchGreen">
+          <div className="OffersPatchLignOne"> VOTRE STUDIO
+          </div>
+          <div className="OffersPatchLignTwo">
+            <span className="Lign"> </span>
+            <div> À PARTIR DE </div>
+            <span className="Lign"> </span>
+          </div>
+          <div className="OffersPatchLignThree">
+            <div className="OffersPatchLignThreePrice"> 390€ </div>
+            <div className="OffersPatchLignThreePerMonths"> /MOIS </div>
+          </div>
+        </div>
+        <div className="OffersPatchTurquoize">
+          <div className="OffersPatchLignOne"> VOTRE 2 PIECES </div>
+          <div className="OffersPatchLignTwo">
+            <span className="Lign"> </span>
+            <div> À PARTIR DE </div>
+            <span className="Lign"> </span>
+          </div>
+          <div className="OffersPatchLignThree">
+            <div className="OffersPatchLignThreePrice"> 570 € </div>
+            <div className="OffersPatchLignThreePerMonths"> /MOIS </div>
+          </div>
+        </div>
+        <div className="OffersPatchPurple">
+          <div className="OffersPatchLignOne"> VOTRE 3 PIECES </div>
+          <div className="OffersPatchLignTwo">
+            <span className="Lign"> </span>
+            <div className="OffersPatchLignTwo"> À PARTIR DE </div>
+            <span className="Lign"> </span>
+          </div>
+          <div className="OffersPatchLignThree">
+            <div className="OffersPatchLignThreePrice"> 814 € </div>
+            <div className="OffersPatchLignThreePerMonths"> /MOIS </div>
+          </div>
+        </div>
+        <div className="OffersPatchOrange">
+          <div className="OffersPatchLignOne"> VOTRE 4 PIECES </div>
+          <div className="OffersPatchLignTwo">
+            <span className="Lign"> </span>
+            <div className="OffersPatchLignTwo"> À PARTIR DE </div>
+            <span className="Lign"> </span>
+          </div>
+          <div className="OffersPatchLignThree">
+            <div className="OffersPatchLignThreePrice"> 972 € </div>
+            <div className="OffersPatchLignThreePerMonths"> /MOIS </div>
+          </div>
+        </div>
+			</Slider>
+			)
+	}
+}
 
 const OffersPatch = () => (
   <div>
-    <MediaQuery minWidth={1224}>
-      <MediaQuery minWidth={769}>
+     <MediaQuery minWidth={1224}>
+      <MediaQuery minWidth={768}>
       <section className="MyContainer OffersPatchGlobal">
         <div className="OffersPatchGreen">
           <div className="OffersPatchLignOne"> VOTRE STUDIO
@@ -63,23 +139,12 @@ const OffersPatch = () => (
         </div>
       </section>
      </MediaQuery>
-    </MediaQuery>
-      <MediaQuery minResolution="2dppx">
-        <div>
-          <div className="OffersPatchTurquoize">
-            <div className="OffersPatchLignOne"> VOTRE 2 PIECES </div>
-            <div className="OffersPatchLignTwo">
-              <span className="Lign"> </span>
-              <div> À PARTIR DE </div>
-              <span className="Lign"> </span>
-            </div>
-            <div className="OffersPatchLignThree">
-              <div className="OffersPatchLignThreePrice"> 570 € </div>
-              <div className="OffersPatchLignThreePerMonths"> /MOIS </div>
-            </div>
-          </div>
+     </MediaQuery>
+      <Mobile>
+        <div className="OffersPatchMobile">
+          <PatchsSlider />
         </div>
-    </MediaQuery>
+      </Mobile>
   </div>
 );
 
